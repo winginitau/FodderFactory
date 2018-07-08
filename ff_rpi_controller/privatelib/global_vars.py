5545e3b868d21c4eee7cbfcfc9e42bec454c7abc
Brendan McLearie <bmclearie@mclearie.com>
2018-07-08 23:25:03 +1000

    rpi version release
    Signed-off-by: Brendan McLearie <bmclearie@mclearie.com>

---
+++ ff_rpi_controller/privatelib/global_vars.py
@@ -0,0 +1,15 @@
+'''
+Created on 8 Jul. 2018
+
+@author: brendan
+'''
+
+from privatelib.msg_parser import MessageSystem
+from kivy.uix.screenmanager import ScreenManager, SwapTransition
+
+#########################################################################
+# Global Vars
+#########################################################################
+
+msg_sys = MessageSystem();
+sm = ScreenManager(transition=SwapTransition())
